90a30b0d9d755ed9a7433487597219a9a6602d8d
dlwldnd <kajima77@naver.com>
2023-04-05 19:18:16 +0900

    오타 수정

--- src/main/java/com/shop/entity/BaseEntity.java
+++ src/main/java/com/shop/entity/BaseEntity.java
@@ -15,7 +15,7 @@ public abstract class BaseEntity extends BaseTimeEntity{
 
     @CreatedBy //생성자 자동저장
     @Column(updatable = false)
-    private String createBy;
+    private String createdBy;
 
     @LastModifiedBy //수정자
     private String modifiedBy;
